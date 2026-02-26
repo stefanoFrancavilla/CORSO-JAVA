da6c9d9d29003590d2549f1df6e519153fb8c4a5
stefanoFrancavilla <francavilla.stefano86@gmail.com>
2026-02-26 19:24:36 +0100

    lezione 15

--- IncapsulamentoDeiDati/src/incapsulamentoDeiDati/Gatto.java
+++ IncapsulamentoDeiDati/src/incapsulamentoDeiDati/Gatto.java
@@ -12,10 +12,18 @@ public class Gatto {
 	private ColorePeloGatto colorePeloGatto;
 	private RazzaGatto razzaGatto;
 	
+	// Variabili Constanti.
+	private final byte min_anni = 0; 
+	private final byte max_anni = 20; 
+	private final byte default_anni = 3;
 	
+	private final float min_altezza = 7f; 
+	private final float max_altezza = 50f; 
+	private final float default_altezza = 15f; 
 	
-	
-	
+	private final float min_peso = 35f; 
+	private final float max_peso = 600f; 
+	private final float default_peso = 40f; 
 
 	//Costruttore.
 	public Gatto() {
@@ -34,7 +42,38 @@ public class Gatto {
 		}
 
 		public void setNome(String nome) {
+			boolean nomeCorretto = true;
+			
+			if (verificaCratteri("1234567890!?@", nome)) 
+			{
+				System.out.println("nome non consono"); 
+				nomeCorretto = false;
+			}
+			else if ( verificaLunghezza(nome, 20))
+			{
+				System.out.println("nome troppo lungo");
+				
+				nomeCorretto = false;
+			}
+			if(nomeCorretto == true) 
+			{
+				System.out.println("nome ok");
+				this.nome = nome;
+			}
+			if ( this.nome == null)
+			{
+				System.out.println(this.nome + " è null ");
+				
+				this.nome = "ND"; 
+			}
+			else if (!("ND".equals(this.nome))) 
+				
+				
+			{
+				System.out.println(this.nome + " è un nome sensato .");
+			}
 			this.nome = nome;
+			
 		}
 
 		public Sesso getSesso() {
@@ -50,6 +89,25 @@ public class Gatto {
 		}
 
 		public void setAnni(byte anni) {
+			System.out.println("max anni: " + max_anni);
+			
+			if(anni >= min_anni && anni <= max_anni) 
+			{
+				this.anni = anni;
+				System.out.println(this.anni + " anni inizializzati");
+			}
+			else
+			{
+				if (this.anni >= min_anni && this.anni <= max_anni) 
+				{
+					System.out.println(this.anni + " anni validi da prima.");
+				}
+				else
+				{
+					this.anni = default_anni; 
+					System.out.println(this.anni + " anni assegnati di default.");
+				}
+			}
 			this.anni = anni;
 		}
 
@@ -58,6 +116,25 @@ public class Gatto {
 		}
 
 		public void setAltezza(float altezza) {
+			if(altezza > min_altezza && altezza < max_altezza) {
+				this.altezza = altezza;
+				System.out.println(this.altezza + " altezza inizializzata");
+			}
+			
+			else
+			{
+				
+				if (this.altezza > min_altezza && this.altezza < max_altezza)
+				{
+					System.out.println(this.altezza + " altezza valida da prima.");
+				}
+			
+				else
+				{
+					this.altezza = default_altezza; 
+					System.out.println(this.altezza + " altezza assegnata di default.");
+				}	
+			}
 			this.altezza = altezza;
 		}
 
@@ -66,6 +143,23 @@ public class Gatto {
 		}
 
 		public void setPeso(float peso) {
+			if(peso > min_peso && peso < max_peso)
+			{
+				this.peso = peso;
+				System.out.println(this.peso + " peso inizializzato");
+			}
+			else
+			{
+				if (this.peso > min_peso && this.peso < max_peso)
+				{
+					System.out.println(this.peso + " peso valido da prima.");
+				}
+				else
+				{
+					this.peso = default_peso; 
+					System.out.println(this.peso + " peso assegnato di default.");
+				}
+			}
 			this.peso = peso;
 		}
 
@@ -128,5 +222,29 @@ public class Gatto {
 		System.out.println("----------");
 	    
 	 }
-
+	
+	//metodi di verifica
+	
+	private boolean verificaCratteri(String caratteri, String s) {
+		boolean presente = false;
+		for (int i = 0; i < caratteri.length(); i++) 
+		{
+			char carattereCorrente = caratteri.charAt(i);
+			System.out.println("carattereCorrente" +  carattereCorrente);
+			if (s.indexOf(caratteri.charAt(i)) != -1)
+			{
+				presente = true;
+				break;
+			}
+	}
+		return presente;
+}
+	private boolean verificaLunghezza(String s, int  limit)
+	{
+		boolean troppoLunga = false;
+		
+		if (s.length() > limit) 
+		    troppoLunga = true;
+		return troppoLunga;
+	}
 }
