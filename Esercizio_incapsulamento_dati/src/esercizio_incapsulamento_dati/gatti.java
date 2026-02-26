c2e33c53c5963d04c11e1edd3f5dee07c770bd1b
stefanoFrancavilla <francavilla.stefano86@gmail.com>
2026-02-26 22:11:12 +0100

    esercizio gatti

--- Esercizio_incapsulamento_dati/src/esercizio_incapsulamento_dati/gatti.java
+++ Esercizio_incapsulamento_dati/src/esercizio_incapsulamento_dati/gatti.java
@@ -2,40 +2,178 @@ package esercizio_incapsulamento_dati;
 
 public class gatti {
 
-	String nome;
-	Esercizio_Sesso sesso;
-	Esercizio_ColoreOcchi coloreOcchi;
-	Colore_cani colorePelo;
-	int eta;
+	private String nome;
+	private byte eta;
+	private float altezza;
+	private float peso;
+	private Esercizio_Sesso sesso;
+	private Esercizio_ColoreOcchi coloreOcchi;
 	
-gatti() {
-	this.nome = nome;
-	this.sesso = sesso;
-	this.coloreOcchi = coloreOcchi;
-	this.colorePelo = colorePelo;
-	this.eta = eta;
-}
+	//costanti
+	
+	private final byte eta_min = 0;
+	private final byte eta_max = 20;
+	private final byte eta_default = 3;
+	
+	private final float altezza_min = 0;
+	private final float altezza_max = 40;
+	private final float altezza_default = 20;
+	
+	private final float peso_min = 0;
+	private final float peso_max = 30;
+	private final float peso_default = 7;
+	
+	
+	//costruttore
+	
+	public gatti()
+	{
+		System.out.println("costruttore (0) parametri");
+	}
+	
+	//metodi setter e getter
+	
+	public String getNome() {
+		return nome;
+	}
 
-	public void miagola() {
-		System.out.println(nome + " miagola");
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public byte getEta() {
+		return eta;
+	}
+
+	public void setEta(byte eta) {
+		
+		if (eta <= eta_min && eta >= eta_max)
+		{
+			System.out.println("età non valida");
+			this.eta = eta_default;
 		}
-	public void mangia() {
-		System.out.println(nome + " mangia");
+		else
+		{
+			System.out.println("età valida");
+			this.eta = eta;
 		}
-	public void dorme() {
-		System.out.println(nome + " dorme");
+		
+	}
+
+	public float getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(float altezza) {
+		
+		if (altezza <= altezza_min && altezza >= altezza_max)
+		{
+			System.out.println("altezza non valida");
+			this.altezza = altezza_default;
 		}
-	public void gioca() {
-		System.out.println(nome + " gioca");
+		else
+		{
+			System.out.println("altezza valida");
+			this.altezza = altezza;
 		}
-	public void stampa() {
-		System.out.println("----------");
-		System.out.println("Nome: " + nome);
-		System.out.println("Sesso: " + sesso);
-		System.out.println("Colore Occhi: " + coloreOcchi);
-		System.out.println("Colore Pelo: " + colorePelo);
-		System.out.println("Età: " + eta);
+		
+		
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		
+		if (altezza <= altezza_min && altezza >= altezza_max)
+		{
+			System.out.println("peso non valido");
+			this.peso = peso_default;
 		}
+		else
+		{
+			System.out.println("peso valido");
+			this.peso = peso;
+		}
+		
+	}
+
+	public Esercizio_Sesso getSesso() {
+		return sesso;
+	}
+
+	public void setSesso(Esercizio_Sesso sesso) {
+		this.sesso = sesso;
+	}
+
+	public Esercizio_ColoreOcchi getColoreOcchi() {
+		return coloreOcchi;
+	}
+
+	public void setColoreOcchi(Esercizio_ColoreOcchi coloreOcchi) {
+		this.coloreOcchi = coloreOcchi;
+	}
+	
+	
+	//metodi
+	
+	public void mangia()
+	{
+		System.out.println("Sta mangiando...");
+	}
+	
+	public void dorme()
+	{
+		System.out.println("Sta dormendo...");
+	}
+	
+	public void gioca()
+	{
+		System.out.println("Sta giocando...");
+	}
+	
+	public void stampa()
+	{
+		System.out.println("--------------------");
+		System.out.println("nome " + nome);
+		System.out.println("età " + eta);
+		System.out.println("altezza " + altezza);
+		System.out.println("peso " + peso);
+		System.out.println("sesso " + sesso);
+		System.out.println("colore occhi " + coloreOcchi);
+		System.out.println("--------------------");
+	}
+	
+	//funzioni di controllo
+	
+	public boolean verificaCaratteri(String caratteri, String s) {
+		boolean presente = true;
+		
+	    for(int i = 0; i < caratteri.length(); i++) {
+	    	
+	        char carattereCorrente = caratteri.charAt(i);
+	        
+	        if (s.indexOf(carattereCorrente) == -1) 
+	        {  
+	           presente = false;  
+	        }
+	    }
+	    return presente;  
+	}
+	
+	public boolean verificaLunghezza(String s, int limit)
+	{
+		boolean parolaLunga = false;
+		
+		if (s.length() > limit)
+		
+			parolaLunga = true;
+		    return parolaLunga;
+	}
+
+
+	
 	}
 	
 
--- Esercizio_incapsulamento_dati/src/esercizio_incapsulamento_dati/mondo_Esercizio.java
+++ Esercizio_incapsulamento_dati/src/esercizio_incapsulamento_dati/mondo_Esercizio.java
@@ -7,36 +7,47 @@ public class mondo_Esercizio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner scanner = new Scanner(System.in);
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		//EssereUmano2 umano1 = new EssereUmano2 ("Giovanna", "D'agnano", (byte) 36 , 165f, 65 );
+//		//umano1.stampa();
+//		
+//	
+//		System.out.println("Inserisci il tuo nome");
+//		
+//		String nome = scanner.nextLine();
+//		
+//		System.out.println("Inserisci il tuo cognome");
+//		
+//		String cognome = scanner.nextLine();
+//		
+//		System.out.println("Inserisci la tua età");
+//		
+//		byte eta = scanner.nextByte();
+//		scanner.nextLine();
+//		
+//		System.out.println("Inserisci la tua altezza");
+//		
+//		float altezza = scanner.nextFloat();
+//		scanner.nextLine();
+//		
+//		System.out.println("Inserisci il tuo peso");
+//		float peso = scanner.nextFloat();
+//		scanner.nextLine();
+//		
+//		EssereUmano3 umano2 = new EssereUmano3(nome, cognome, eta, altezza, peso);
+//		umano2.stampa();
 		
-		//EssereUmano2 umano1 = new EssereUmano2 ("Giovanna", "D'agnano", (byte) 36 , 165f, 65 );
-		//umano1.stampa();
+		gatti micio = new gatti();
 		
-	
-		System.out.println("Inserisci il tuo nome");
+		micio.setNome("micio");
+		micio.setAltezza(32);
+		micio.setPeso(10);
+		micio.setEta((byte)4);
+		micio.setSesso(Esercizio_Sesso.MASCHIO);
+		micio.setColoreOcchi(Esercizio_ColoreOcchi.GRIGI);
 		
-		String nome = scanner.nextLine();
-		
-		System.out.println("Inserisci il tuo cognome");
-		
-		String cognome = scanner.nextLine();
-		
-		System.out.println("Inserisci la tua età");
-		
-		byte eta = scanner.nextByte();
-		scanner.nextLine();
-		
-		System.out.println("Inserisci la tua altezza");
-		
-		float altezza = scanner.nextFloat();
-		scanner.nextLine();
-		
-		System.out.println("Inserisci il tuo peso");
-		float peso = scanner.nextFloat();
-		scanner.nextLine();
-		
-		EssereUmano3 umano2 = new EssereUmano3(nome, cognome, eta, altezza, peso);
-		umano2.stampa();
+		micio.stampa();
 	}
 
 }
