a71b7e11839811ad2408ef099b748b2645b7a2dc
stefanoFrancavilla <francavilla.stefano86@gmail.com>
2026-04-21 11:48:05 +0200

    esercizio singleton

--- EsercizioSingleton2/src/esercizioSiingleton2/CentraleTaxi.java
+++ EsercizioSingleton2/src/esercizioSiingleton2/CentraleTaxi.java
@@ -1,5 +1,73 @@
 package esercizioSiingleton2;
 
 public class CentraleTaxi {
+	
+	private static int taxiDisponibili = 4;
+	private static int corseTotali = 0;
+	private static boolean sistemaOccupato = false;
+	private static String nomeCentrale = "Centrale Taxi";
+    private static String citta = "Bellinzona";
+    
+    //costruttore 
+    private CentraleTaxi()
+	{	
+    	nomeCentrale = "Centrale Taxi";
+    	citta = "Bellinzona";
+    		}
+	
+	public static CentraleTaxi getInstance() {
+		return CentraleTaxiBuilder.INSTANCE;
+	}
+	
+	private static class CentraleTaxiBuilder
+	{
+		private static final CentraleTaxi INSTANCE = new CentraleTaxi();
+	}
 
-}
+    //metodi getter e setter
+	
+	public static int getTaxiDisponibili() {
+		return taxiDisponibili;
+	}
+
+	public static void setTaxiDisponibili(int taxiDisponibili) {
+		CentraleTaxi.taxiDisponibili = taxiDisponibili;
+	}
+
+	public static int getCorseTotali() {
+		return corseTotali;
+	}
+
+	public static void setCorseTotali(int corseTotali) {
+		CentraleTaxi.corseTotali = corseTotali;
+	}
+
+	public static boolean isSistemaOccupato() {
+		return sistemaOccupato;
+	}
+
+	public static void setSistemaOccupato(boolean sistemaOccupato) {
+		CentraleTaxi.sistemaOccupato = sistemaOccupato;
+	}
+
+	public static String getNomeCentrale() {
+		return nomeCentrale;
+	}
+
+	public static void setNomeCentrale(String nomeCentrale) {
+		CentraleTaxi.nomeCentrale = nomeCentrale;
+	}
+
+	public static String getCitta() {
+		return citta;
+	}
+
+	public static void setCitta(String citta) {
+		CentraleTaxi.citta = citta;
+	}
+    
+
+	
+	
+    
+    }
--- EsercizioSingleton2/src/esercizioSiingleton2/Cliente.java
+++ EsercizioSingleton2/src/esercizioSiingleton2/Cliente.java
@@ -1,5 +1,32 @@
 package esercizioSiingleton2;
 
-public class Cliente {
+public class Cliente extends Thread{
 
+	//variabili d'istanza
+	public static String nome;
+	
+	//costruttore
+	public Cliente(String nome)
+	{
+		this.nome = nome;
+	}
+
+
+	
+	//metodi getter e setter
+	
+	public static String getNome() {
+		return nome;
+	}
+
+	public static void setNome(String nome) {
+		Cliente.nome = nome;
+	}
+	
+	@Override
+	public void run()
+	{
+		
+	}
+	
 }
