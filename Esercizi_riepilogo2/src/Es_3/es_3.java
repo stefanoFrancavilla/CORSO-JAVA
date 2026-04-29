c79dc774eb9f9315db9b19d529243ff12b2f38d6
stefanoFrancavilla <francavilla.stefano86@gmail.com>
2026-04-29 17:03:48 +0200

    esercizi di riepilogo

--- Esercizi_riepilogo2/src/Es_3/es_3.java
+++ Esercizi_riepilogo2/src/Es_3/es_3.java
@@ -9,20 +9,45 @@ public class es_3 {
 		 * Esercizio 3: Media Voti con Approvazione
            Array 7 double (voti). Calcola media con metodo. Conta promossi (>=6) e stampa "Classe promossa" se media>6.
 		 */
+		double contaPromossi = 0;
 		
 		double [] voti = new double[7];
 		
+		for (int i=0; i<voti.length; i++)
+		{
+			voti[i] = Math.random() * 10; 
+			if (voti[i] >= 6)
+			{
+				contaPromossi++;
+			}
+		}
+		
+		if (media(voti) >= 6)
+		{
+			System.out.println("Numero di promossi: " + contaPromossi);
+			System.out.println("Media: " + media(voti));
+			System.out.println("Classe promossa");
+		}
+		 else
+		 {
+			 System.out.println("Numero di promossi: " + contaPromossi);
+		     System.out.println("Media: " + media(voti));
+			 System.out.println("Classe bocciata");
+		 }
+		 
 		
 		
 	}
 	
-	public static double media(double[] voti, double studenti)
+	public static double media(double [] voti)
 	{
 		double mediaVoti = 0;
-		
-		
-		
+		for(int i=0; i<voti.length; i++)
+		{
+			mediaVoti += voti[i];
+		}
+		mediaVoti = mediaVoti / voti.length;
 		return mediaVoti;
 	}
-
+//voto 9/10
 }
--- Esercizi_riepilogo2/src/Es_4/es_4.java
+++ Esercizi_riepilogo2/src/Es_4/es_4.java
@@ -1,5 +1,8 @@
 package Es_4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class es_4 {
 
 	public static void main(String[] args) {
@@ -9,7 +12,26 @@ public class es_4 {
 		 * Esercizio 4: Ricerca Valore Max
            Array 8 int. Trova posizione massimo con ciclo. Se multipli, stampa tutte le posizioni.
 		 */
+		Random random = new Random();
+		int[] array = new int[8];
 		
-	}
+		   int max = 0;
+	        for (int i = 0; i < array.length; i++) {
+	            array[i] = random.nextInt(100);
+	            max = Math.max(max, array[i]);
+	        }
 
-}
+	        System.out.println("Massimo: " + max);
+	        System.out.println(Arrays.toString(array));
+
+	   
+	        System.out.print("Posizioni del massimo: ");
+	        for (int i = 0; i < array.length; i++) {
+	            if (array[i] == max) {
+	                System.out.print(i + " ");
+	            }
+	        }
+	        System.out.println(); 
+	    }
+	//voto 9/10
+	}
